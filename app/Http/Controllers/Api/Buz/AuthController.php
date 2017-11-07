d73229aaeade03ec823c546b3012f2bc95d5e141
deandong <844199574@qq.com>
2017-11-07 21:53:28 +0800

    add doregister view

--- app/Http/Controllers/Api/Buz/AuthController.php
+++ app/Http/Controllers/Api/Buz/AuthController.php
@@ -2,11 +2,13 @@
 namespace App\Http\Controllers\Api\Buz;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\DoRegisterEmail;
 use App\Lib\Code;
 use App\Models\PlatUser;
 use App\Models\PlatUserTmp;
 use App\Services\ApiResponseService;
 use Carbon\Carbon;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Ramsey\Uuid\Uuid;
@@ -16,6 +18,8 @@ use Tymon\JWTAuth\Facades\JWTFactory;
 
 class AuthController extends Controller
 {
+    use DispatchesJobs;
+
     public function login(Request $request)
     {
         $credentials = $request->only('username', 'password');
@@ -40,16 +44,23 @@ class AuthController extends Controller
         $token = md5($expired_at . $tmp['username'] . $tmp['phone']);
         $tmp += compact('token', 'expired_at');
         if (! $tmpUser = PlatUserTmp::where('username', $tmp['username'])->first()) {
-            PlatUserTmp::create($tmp);
+            $userTmp = PlatUserTmp::create($tmp);
         } else {
-            PlatUserTmp::where('username', $tmp['username'])->update($tmp);
+            $userTmp = PlatUserTmp::where('username', $tmp['username'])->update($tmp);
         }
+        $this->dispatch(new DoRegisterEmail($userTmp));
         return ApiResponseService::success(Code::SUCCESS);
     }
 
     public function doRegister(Request $request)
     {
-
+        $data = $request->all();
+        $userTmp = PlatUserTmp::where('token', $data['token'])
+            ->where('username', $data['username'])
+            ->first();
+        if ($userTmp) {
+            
+        }
     }
 
     public function getAuthUser(Request $request)
--- app/Http/Controllers/Buz/AuthController.php
+++ app/Http/Controllers/Buz/AuthController.php
@@ -2,6 +2,8 @@
 namespace App\Http\Controllers\Buz;
 
 use App\Http\Controllers\Controller;
+use App\Models\PlatUserTmp;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -10,10 +12,13 @@ class AuthController extends Controller
 
     }
 
-    public function doRegister()
+    public function doRegister(Request $request)
     {
-
-
+        $credentials = $request->only('token', 'username');
+        $tmp = PlatUserTmp::where($credentials)->where('expired_at', '>=', time())->first();
+        return view('buz.auth.doRegister', [
+            'tmp' => $tmp,
+        ]);
     }
 
     public function login()
